2c08f5626f463d0a345fb76c551dd48c1c871168
LMini562 <71384352+lmini562@users.noreply.github.com>
2021-09-30 14:20:35 +0300

    Create 1.cs

---
+++ 1lab/1.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class oneone : MonoBehaviour
+{
+ private Rigidbody _rigidbody;
+ private Vector3 _move;
+ [SerializeField] private float _S;
+ private float _T = 0.0f, _dist = 0.0f;
+ // Start is called before the first frame update
+ void Start()
+ {
+ _rigidbody = GetComponent<Rigidbody>();
+ InvokeRepeating("Output", 0f, 2f);
+ }
+ // Update is called once per frame
+ void Update()
+ {
+ _move = new Vector3(0, 0, 1);
+ _rigidbody.position = _rigidbody.position + _move * _S * Time.deltaTime;
+ _dist += Time.deltaTime * Mathf.Abs(_S);
+ _T += Time.fixedDeltaTime;
+ }
+ private void Output()
+ {
+ Debug.Log("Object speed = " + _S + ", Time = " + (int)_T + ", Distance = " + 
+Mathf.Round(_dist));
+ }
+}
